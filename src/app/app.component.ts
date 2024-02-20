e9d312542c2e192db828f6536ee3f192575130c5
ITboo <annaacdc@tut.by>
2024-02-20 15:15:26 +0300

    feat:implement task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,100 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'burgers';
+  currencyData = {
+    dollar: {
+      sign: '$',
+      coefficient: 1,
+    },
+    ruble: {
+      sign: '₽',
+      coefficient: 90,
+    },
+    belRub: {
+      sign: 'BYN',
+      coefficient: 3,
+    },
+    euro: {
+      sign: '€',
+      coefficient: 0.9,
+    },
+    yuan: {
+      sign: '¥',
+      coefficient: 6.9,
+    },
+  }
+
+  currency = this.currencyData.dollar.sign;
+
+  form = this.fb.group({
+    order: ['', Validators.required],
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+  })
+
+  productsData: any;
+
+  constructor(private fb: FormBuilder, private appService: AppService) {
+  }
+
+  ngOnInit() {
+    this.appService.getData().subscribe((data: any) => this.productsData = data);
+  }
+
+  scrollTo(target: HTMLElement, product?: any) {
+    target.scrollIntoView({behavior: 'smooth'});
+    if (product) {
+      this.form.patchValue({order: `${product.title} (${product.price} ${this.currency})`});
+    }
+  }
+
+  confirmOrder() {
+    if (this.form.valid) {
+      this.appService.sendOrder(this.form.value)
+        .subscribe({
+          next: (response: any) => {
+            alert(response.message)
+          },
+          error: (response: any) => {
+            alert(response.error.message)
+          },
+        });
+
+      this.form.reset();
+    }
+  }
+
+  changeCurrency() {
+    let newCurrency = this.currencyData.dollar.sign;
+    let coefficient = this.currencyData.dollar.coefficient;
+
+    if (this.currency === this.currencyData.dollar.sign) {
+      newCurrency = this.currencyData.ruble.sign;
+      coefficient = this.currencyData.ruble.coefficient;
+    } else if (this.currency === this.currencyData.ruble.sign) {
+      newCurrency = this.currencyData.belRub.sign;
+      coefficient = this.currencyData.belRub.coefficient;
+    } else if (this.currency === this.currencyData.belRub.sign) {
+      newCurrency = this.currencyData.euro.sign;
+      coefficient = this.currencyData.euro.coefficient;
+    } else if (this.currency === this.currencyData.euro.sign) {
+      newCurrency = this.currencyData.yuan.sign;
+      coefficient = this.currencyData.yuan.coefficient;
+    }
+
+    this.currency = newCurrency;
+
+    this.productsData.forEach((product: any) => {
+      const totalPrice = product.basePrice * coefficient;
+      product.price = Number.isInteger(totalPrice) ? totalPrice : +totalPrice.toFixed(1);
+    });
+
+  }
 }
